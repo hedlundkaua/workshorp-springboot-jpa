048814e7de4828338c7e81a9b2624ed06a19bd21
hedlundkaua <hedlundkaua@gmail.com>
2025-06-02 14:32:50 -0300

    Project created

---
+++ src/main/java/com/hedlundkaua/course/CourseApplication.java
@@ -0,0 +1,16 @@
+package com.hedlundkaua.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //configurar o projetopara que seja um projeto Spring boot
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	
+	
+	
+	}
+
+}
