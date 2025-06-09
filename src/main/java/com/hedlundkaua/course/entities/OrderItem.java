aa98429deb4dfc04b15e6cdb3bc22c029b5f39e8
hedlundkaua <hedlundkaua@gmail.com>
2025-06-09 14:59:11 -0300

    Subtotal & Total methods

--- src/main/java/com/hedlundkaua/course/entities/OrderItem.java
+++ src/main/java/com/hedlundkaua/course/entities/OrderItem.java
@@ -67,6 +67,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
@@ -84,7 +88,4 @@ public class OrderItem implements Serializable{
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 }
