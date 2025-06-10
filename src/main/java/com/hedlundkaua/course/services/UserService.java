ce897918669c5e58f8ddef93e9d17276266aafc4
hedlundkaua <hedlundkaua@gmail.com>
2025-06-10 11:38:36 -0300

    Exception handling - update

--- src/main/java/com/hedlundkaua/course/services/UserService.java
+++ src/main/java/com/hedlundkaua/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.hedlundkaua.course.repositories.UserRepository;
 import com.hedlundkaua.course.services.exceptions.DatabaseException;
 import com.hedlundkaua.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service //instancia como componente do Spring
 public class UserService {
@@ -46,10 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User user = repository.getReferenceById(id); // ele instancia um usuario, mas não vai ao banco de dados ainda, ele deixa o obj monitorado pelo JPA e em seguida podemos efetuar alguma operação no banco de dados.
 		//o findById vai diretamente ao banco de dados 
 		updateData(user, obj); //função
-		return repository.save(user); //salvo no banco de dados
+		return repository.save(user);//salvo no banco de dados
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User user, User obj) {
 		user.setName(obj.getName());
